be15f7ee6a40728d2dbf4d4fc5b9fe910e5b658f
Drew Chase <dcmanproductions@gmail.com>
2023-10-12 07:46:17 -0400

    added test code

--- test-src/main.cpp
+++ test-src/main.cpp
@@ -1,4 +1,20 @@
-int main(void)
+#include "../src/options_manager.h"
+
+int main(int argc, char **argv)
 {
+    cclip::options_manager manager("cclip test");
+    manager.add_option("h", "help", "Print this help message", false, false);
+    manager.add_option("v", "version", "Print the version", false, false);
+    manager.add_option("f", "file", "The file to read", true, true);
+
+    manager.parse(argc, argv);
+
+    if (manager.is_present("h"))
+    {
+        manager.print_help();
+        return 0;
+    }
+
+
     return 0;
 }
