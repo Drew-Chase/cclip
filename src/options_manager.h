13bde400e5e243f54cd6b85eb3f3fe07612960e5
Drew Chase <dcmanproductions@gmail.com>
2023-10-12 08:10:02 -0400

    added documentation header

--- src/options_manager.h
+++ src/options_manager.h
@@ -11,11 +11,29 @@ namespace cclip
 {
     struct option
     {
+        /**
+         * The short name of the option (e.g. -h) minus the dash (-).
+         */
         const char *short_name;
+        /**
+         * The long name of the option (e.g. --help) minus the dashes (--).
+         */
         const char *long_name;
+        /**
+         * The description of the option.
+         */
         const char *description;
+        /**
+         * Whether or not the option is required.
+         */
         bool is_required;
+        /**
+         * Whether or not the option has an argument.
+         */
         bool has_argument;
+        /**
+         * The argument of the option.
+         */
         char *argument;
     };
 
