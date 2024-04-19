fee00ef58f802eea1fc5ca5111b160ef55dccf3a
Drew Chase (Home) <dcmanproductions@gmail.com>
2024-04-18 21:38:02 -0400

    Add pause command to main program
    A "pause" command has been added at the end of main.cpp to halt execution, allowing users or developers to see the final output before the console closes. This essentially makes it easier to debug and visualize the program's behavior.

--- example/main.cpp
+++ example/main.cpp
@@ -2,31 +2,32 @@
 #include "../cclip.hpp"
 #include <iostream>
 
-int main(const int argc, char** argv)
+int main(const int argc, char **argv)
 {
-	cclip::options_manager manager("cclip test");
-	manager.add_option("h", "help", "Print this help message", false, false);
-	manager.add_option("v", "version", "Print the version", false, false);
-	manager.add_option("f", "file", "The file to read", true, true);
+    cclip::options_manager manager("cclip test");
+    manager.add_option("h", "help", "Print this help message", false, false);
+    manager.add_option("v", "version", "Print the version", false, false);
+    manager.add_option("f", "file", "The file to read", true, true);
 
-	manager.parse(argc, argv);
+    manager.parse(argc, argv);
 
-	if (manager.is_present("h"))
-	{
-		manager.print_help();
-		return 0;
-	}
+    if (manager.is_present("h"))
+    {
+        manager.print_help();
+        return 0;
+    }
 
-	if (manager.is_present("v"))
-	{
-		std::cout << "cclip test 0.0.1" << std::endl;
-		return 0;
-	}
+    if (manager.is_present("v"))
+    {
+        std::cout << "cclip test 0.0.1" << std::endl;
+        return 0;
+    }
 
-	if (manager.is_present("f") && manager.get_option("f")->argument != nullptr)
-	{
-		std::cout << "File: " << manager.get_option("f")->argument << std::endl;
-	}
+    if (manager.is_present("f") && manager.get_option("f")->argument != nullptr)
+    {
+        std::cout << "File: " << manager.get_option("f")->argument << std::endl;
+    }
+    system("pause");
 
-	return 0;
+    return 0;
 }
